85f6ffb85150248da4eff42720c62f96a4a0469c
soumy-jain <piyush06jain.nov@gmail.com>
2021-08-20 02:02:01 +0530

    site using react-bootstrap

--- tha25/src/components/Car.jsx
+++ tha25/src/components/Car.jsx
@@ -12,8 +12,7 @@ const Car = () => {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Nature</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -25,8 +24,7 @@ const Car = () => {
           />
       
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Water</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -38,8 +36,7 @@ const Car = () => {
           />
       
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h3>Sky</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
